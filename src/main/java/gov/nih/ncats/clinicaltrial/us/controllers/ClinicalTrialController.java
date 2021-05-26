49a5d4509e86e53d138f933a515eb9b767a428ba
Welsch <Alexander.Welsch@fda.gov>
2021-05-26 10:56:33 -0400

    clean up controller

--- src/main/java/gov/nih/ncats/clinicaltrial/us/controllers/ClinicalTrialController.java
+++ src/main/java/gov/nih/ncats/clinicaltrial/us/controllers/ClinicalTrialController.java
@@ -7,13 +7,6 @@ import gov.nih.ncats.clinicaltrial.us.services.ClinicalTrialLegacySearchService;
 import gov.nih.ncats.clinicaltrial.us.services.SubstanceAPIService;
 import gsrs.controller.*;
 import gsrs.legacy.LegacyGsrsSearchService;
-//import org.hibernate.search.backend.lucene.LuceneExtension;
-//import org.hibernate.search.engine.search.predicate.dsl.BooleanPredicateClausesStep;
-//import org.hibernate.search.engine.search.predicate.dsl.MatchPredicateFieldStep;
-//import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
-//import org.hibernate.search.engine.search.query.SearchResult;
-//import org.hibernate.search.mapper.orm.search.query.dsl.HibernateOrmSearchQuerySelectStep;
-//import org.hibernate.search.mapper.orm.session.SearchSession;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -22,7 +15,6 @@ import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Hashtable;
 import java.util.Map;
@@ -74,20 +66,28 @@ public class ClinicalTrialController extends EtagLegacySearchEntityController<Cl
     protected Stream<ClinicalTrial> filterStream(Stream<ClinicalTrial> stream, boolean publicOnly, Map<String, String> parameters) {
         return stream;
     }
-    @GetGsrsRestApiMapping("/@lf0")
-    public JSONObject sayHello0()
+
+
+
+
+    // The below will be removed.
+
+    // experimental
+    @GetGsrsRestApiMapping("/@exp_updateSmallSampleOfClinicalTrialMetaData")
+    public JSONObject updateSmallSampleOfClinicalTrialMetaData()
     {
         System.out.println("Running Clinical Trials Meta Updater");
         clinicalTrialMetaUpdaterService.download();
-
         Map<String, String> hm = new Hashtable<String, String>();
         hm.put("one", "a");
         hm.put("two", "b");
         return new JSONObject(hm);
     }
 
-    @GetGsrsRestApiMapping("/@lf1")
-  public JSONObject sayHello1()
+
+    // experimental
+    @GetGsrsRestApiMapping("/@exp_updateAllClinicalTrialMetaData")
+  public JSONObject updateAllClinicalTrialMetaData()
   {
       System.out.println("Running Clinical Trials Meta Updater");
       clinicalTrialMetaUpdaterService.download2();
@@ -98,8 +98,9 @@ public class ClinicalTrialController extends EtagLegacySearchEntityController<Cl
       return new JSONObject(hm);
   }
 
-    @GetGsrsRestApiMapping("/@lf2/{uuid}")
-    public JSONObject sayHello2(@PathVariable String uuid) {
+    // experimental
+    @GetGsrsRestApiMapping("/@exp_testSubstanceExists/{uuid}")
+    public JSONObject testSubstanceExists(@PathVariable String uuid) {
         System.out.println("checking if substance exists");
         Boolean b = substanceAPIService.substanceExists(uuid);
         System.out.println("value returned = "+ b);
@@ -109,25 +110,12 @@ public class ClinicalTrialController extends EtagLegacySearchEntityController<Cl
         return new JSONObject(hm);
     }
 
-    @GetGsrsRestApiMapping("/@substancepassthru/{uuid}")
-    public ResponseEntity<String> sayHello3(@PathVariable String uuid) {
-        System.out.println("getting substance json");
-        return substanceAPIService.getSubstanceDetailsFromUUID(uuid);
-    }
-
-    @GetGsrsRestApiMapping("/@substancenamepassthru/search")
-    public ResponseEntity<String> sayHello4( @RequestParam("name") String name)  {
-        System.out.println("getting substance from name json");
-        return substanceAPIService.getSubstanceDetailsFromName(name);
-    }
-
-    @GetGsrsRestApiMapping("/@substanceQuickMatches")
-    public ResponseEntity<?> sayHello5()  {
+    // experimental
+    @GetGsrsRestApiMapping("/@exp_testSubstanceQuickMatches")
+    public ResponseEntity<?> testSubstanceQuickMatches()  {
         System.out.println("getting substance substanceQuickMatches");
         return substanceAPIService.getQuickResultMatchesByUuids();
     }
-
-
 
 
 }
